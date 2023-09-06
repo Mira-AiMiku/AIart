0ab00ec53c0180b75ec629c70460fc3f168b3e36
Macaron <hatsune.miku.macaron@gmail.com>
2023-09-07 00:12:28 +0900

    font

--- script.js
+++ script.js
@@ -6,8 +6,9 @@ var ctx;
 // ctx = canvas.getContext('2d');
 // var canvasImg = document.getElementById("canvas-img");
 // ctx.drawImage(canvasImg, 0, 0);
+var element = document.getElementById('myslider');
 var image = new Image();
-image.src = 'test.png';
+image.src = 'https://mira-aimiku.github.io/AIart/test.png';
 image.onload = (function () {
     //画像ロードが完了してからキャンバスの準備をする
     canvas = document.getElementById('preview');
@@ -22,35 +23,23 @@ image.onload = (function () {
 //キャンバスに文字を描く
 function drawText()
 {
+    var canvasImg = document.getElementById("canvas-img");
     var text = document.getElementById('canvas_text');
     //文字のスタイルを指定
-    ctx.font = '32px serif';
-    ctx.fillStyle = '#404040';
+    ctx.font = element.value+'px IPAMincho';
+    ctx.fillStyle = '#ffffff';
     //文字の配置を指定（左上基準にしたければtop/leftだが、文字の中心座標を指定するのでcenter
-    ctx.textBaseline = 'center';
+    ctx.textBaseline = 'bottom';
     ctx.textAlign = 'center';
     //座標を指定して文字を描く（座標は画像の中心に）
     var x = (canvas.width / 2);
+    x = Math.ceil(canvas.width-(Number(element.value)*2+element.value.length*(Number(element.value)/1)));
     var y = (canvas.height / 2);
+    y = Math.ceil(canvas.height-1.75*Number(element.value)+Number(element.value));
+    console.log(x);
+    console.log(y);
+    ctx.drawImage(canvasImg, 0, 0);
     ctx.fillText(text.value, x, y);
     base64 = canvas.toDataURL("image/png");
     document.getElementById("download").href = base64;
-}
-function share(){
-    // Web Share APIの対応判定
-    var base64 = canvas.toDataURL("image/png");
-    if (navigator.share !== undefined){
-        // CanvasをBlobに変換→pngに変換
-        canvas.toBlob( (blob) => {
-            const shareImg = new File([blob], 'share.png', {type: 'image/png'})
-            // シェア
-            navigator.share({
-                text: "トイプードルのひとこと",
-                url: "https://web-breeze.net/add-text-to-image-and-share/",
-                files: [shareImg]
-            })
-        })
-    } else {
-        alert("ご利用のブラウザがWeb Share APIに対応していません・・・")
-    }
 }
